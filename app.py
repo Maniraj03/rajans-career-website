823953cf06242dee3e7d0de6d6e582fc9a908cab
Maniraj Lakshmanan <90543834+Maniraj03@users.noreply.github.com>
2023-03-30 10:36:15 +0000

    Dynamic data included in the home page

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
 from flask import Flask,render_template
 app = Flask(__name__)
+
+JOBS = [
+{
+  'id':1,
+  'title':'Data Analyst',
+  'location':'Bengaluru,India',
+  'salary':'Rs. 10,00,000'
+  
+},
+  {
+  'id':2,
+  'title':'Data Scientist',
+  'location':'Hydrebad,India',
+  'salary':'Rs. 15,00,000'
+},
+  {
+  'id':3,
+  'title':'Business Analyst',
+  'location':'Chennai,India',
+  'salary':'Rs. 12,00,000'
+},
+  {
+  'id':4,
+  'title':'Backend Engineer',
+  'location':'California,USA',
+  'salary':'$120,000'
+}
+  
+]
 @app.route("/")
 def hello_rajan():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS)
+
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
